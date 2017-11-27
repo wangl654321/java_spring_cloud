f66a828189ee90bae543c62e932e32f9646995a8
wangl <lwang18@mail.greenriver.edu>
2017-11-27 17:22:53 +0800

    新增断路器（Hystrix）和网管zuul项目代码

---
+++ service_zuul/src/main/java/service/zuul/service_zuul/MyFilter.java
@@ -0,0 +1,69 @@
+package service.zuul.service_zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/***
+ *
+ *
+ * 描    述：
+ *
+ * 创 建 者： wangl
+ * 创建时间：  2017-11-2716:48
+ * 创建描述：
+ *
+ * 修 改 者：  
+ * 修改时间： 
+ * 修改描述： 
+ *
+ * 审 核 者：
+ * 审核时间：
+ * 审核描述：
+ *
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+            }
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
